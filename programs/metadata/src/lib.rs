e38e7903165be6357ea86352cc8a118528082efc
Sebastian Bor <Sebastian_Bor@hotmail.com>
2024-03-20 18:39:43 +0000

    chore: Generate program scaffolding

---
+++ programs/metadata/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6qNJgnGrx2tnvmXEgNCpCCe7yfe4tNRcUUQSZZnV9WhC");
+
+#[program]
+pub mod metadata {
+    use super::*;
+
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
