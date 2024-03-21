84d4be51367213849beddcd11fc048af7589720b
Sebastian Bor <Sebastian_Bor@hotmail.com>
2024-03-21 17:20:27 +0000

    wip: Change Owner to Metadata authority

--- programs/metadata/src/state.rs
+++ programs/metadata/src/state.rs
@@ -62,7 +62,7 @@ pub struct MetadataCollection {
 
 /// Metadata account defines a set of metadata items
 ///
-// PDA seeds: 'metadata' + [metadata_key_id] + [subject] + [owner_authority]
+// PDA seeds: 'metadata' + [metadata_key_id] + [subject] + [metadata_authority]
 pub struct Metadata {
     /// Unique identifier of the MetadataKey describing the collection
     pub metadata_key_id: u64,
@@ -73,7 +73,7 @@ pub struct Metadata {
     /// The authority which created the Metadata account and owns it
     /// Note: The authority is embedded in the PDA seeds and cannot be changed
     /// If a new authority is required then a new Metadata account must be created
-    pub owner_authority: Pubkey,
+    pub metadata_authority: Pubkey,
 
     /// The default update authority for all the collections
     /// Note: The authority can be overridden at the collection level
