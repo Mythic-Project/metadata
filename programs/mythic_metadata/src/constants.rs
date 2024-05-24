fd20e122680c1e232dc901e62cf2119f2273148b
Sebastian Bor <Sebastian_Bor@hotmail.com>
2024-05-24 14:25:55 +0300

    fix: Increase content-type-max length

--- programs/mythic_metadata/src/constants.rs
+++ programs/mythic_metadata/src/constants.rs
@@ -6,6 +6,6 @@ pub const METADATA: &[u8] = b"metadata";
 pub const MAX_NAME_LEN: usize = 50;
 pub const MAX_LABEL_LEN: usize = 30;
 pub const MAX_DESCRIPTION_LEN: usize = 100;
-pub const MAX_CONTENT_TYPE_LEN: usize = 20;
+pub const MAX_CONTENT_TYPE_LEN: usize = 30;
 pub const MAX_VALUE_LEN: usize = 100;
 pub const MAX_COLLECTIONS_PER_METADATA: usize = 5;
