f4505c54b3c585b43135a8e89faa07a670f20ea6
Sebastian Bor <Sebastian_Bor@hotmail.com>
2024-05-24 14:23:55 +0300

    fix: Change label and name max lengths
    The name should be technical, unique and descriptive and label is user friendly

--- programs/mythic_metadata/src/constants.rs
+++ programs/mythic_metadata/src/constants.rs
@@ -3,8 +3,8 @@ pub const COUNTER: &[u8] = b"counter";
 pub const METADATA_KEY: &[u8] = b"metadata_key";
 pub const METADATA: &[u8] = b"metadata";
 
-pub const MAX_NAME_LEN: usize = 30;
-pub const MAX_LABEL_LEN: usize = 50;
+pub const MAX_NAME_LEN: usize = 50;
+pub const MAX_LABEL_LEN: usize = 30;
 pub const MAX_DESCRIPTION_LEN: usize = 100;
 pub const MAX_CONTENT_TYPE_LEN: usize = 20;
 pub const MAX_VALUE_LEN: usize = 100;
