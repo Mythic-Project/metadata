e3bef6d581f8cbeacfe2adee5d6369d05dca475d
Sebastian Bor <Sebastian_Bor@hotmail.com>
2024-05-24 15:29:28 +0300

    fix: Increase Value Len to 10k

--- programs/mythic_metadata/src/constants.rs
+++ programs/mythic_metadata/src/constants.rs
@@ -7,5 +7,5 @@ pub const MAX_NAME_LEN: usize = 50;
 pub const MAX_LABEL_LEN: usize = 30;
 pub const MAX_DESCRIPTION_LEN: usize = 100;
 pub const MAX_CONTENT_TYPE_LEN: usize = 30;
-pub const MAX_VALUE_LEN: usize = 100;
+pub const MAX_VALUE_LEN: usize = 10000;
 pub const MAX_COLLECTIONS_PER_METADATA: usize = 5;
